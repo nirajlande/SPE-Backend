3ded8966ac0f008b1c8bc9a18d2ecd562b242504
Niraj <nirajlande419@gmail.com>
2023-05-11 22:45:12 +0530

    test

---
+++ src/main/java/com/had/selfhelp/controller/CustomerController.java
@@ -0,0 +1,77 @@
+package com.had.selfhelp.controller;
+
+import com.had.selfhelp.entity.Camplaints;
+import com.had.selfhelp.entity.Customer;
+import com.had.selfhelp.entity.LoginRequest;
+import com.had.selfhelp.service.CamplaintServices;
+import com.had.selfhelp.service.CustomerServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerController {
+
+    @Autowired
+    CustomerServices customerServices;
+
+     @Autowired
+    CamplaintServices camplaintServices;
+
+    @PostMapping("/customer")
+    public void save(@RequestBody Customer c ){
+        customerServices.save(c);
+    }
+
+    @PostMapping("/customer/{cust_id}")
+    public List<Camplaints> save(@RequestBody Camplaints c , @PathVariable(name="cust_id")int cust_id)
+    {
+         Customer cust = customerServices.findById(cust_id);
+         c.setCustomer(cust);
+         c.setStatus("Pending");
+         c.setAknow("Pending");
+         camplaintServices.save(c);
+         return customerServices.costumerComplaint(cust);
+    }
+
+    @GetMapping("/complaints/{cust_id}")
+    public List<Camplaints>getcomplaints(@PathVariable(name="cust_id")int cust_id)
+    {
+
+        Customer cust = customerServices.findById(cust_id);
+        if(cust_id==1)
+        {
+          return   camplaintServices.getAll();
+        }
+        else
+            return customerServices.costumerComplaint(cust);
+    }
+    @PostMapping("/login")
+    public Customer validate(@RequestBody LoginRequest log)
+    {
+          return customerServices.login(log);
+    }
+
+    @PostMapping("/owner/status/{com_id}")
+    public void status(@PathVariable(name="com_id")int com_id)
+    {
+        Camplaints c =  camplaintServices.findById(com_id);
+        c.setStatus("Done");
+         camplaintServices.save(c);
+
+    }
+
+    @PostMapping("/owner/aknow/{com_id}/{aknow}")
+    public void status(@PathVariable(name="com_id")int com_id,@PathVariable(name="aknow")String aknow)
+    {
+        Camplaints c =  camplaintServices.findById(com_id);
+        c.setAknow(aknow);
+        camplaintServices.save(c);
+
+    }
+
+
+}
---
+++ src/test/java/com/had/selfhelp/SelfhelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.had.selfhelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfhelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
